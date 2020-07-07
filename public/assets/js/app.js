588151d2a72823f51b37d4dd6c72bc26278c22c2
willhitch <hitch.will@gmail.com>
2020-07-06 19:12:07 -0600

    centered map around denver

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,7 +1,7 @@
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: 39.7392, lng: -104.9903 },
     zoom: 8,
   });
 }
