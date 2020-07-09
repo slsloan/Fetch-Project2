1bbc9e0bcca9c9dd043e714f934214f03dcb5121
Sammy Sloan <smileysammy3@gmail.com>
2020-07-08 20:24:54 -0600

    updated boilerplate

--- server.js
+++ server.js
@@ -16,7 +16,17 @@ app.use(express.json());
 // define static directory
 app.use(express.static("public"));
 
-app.listen(PORT, function () {
-    console.log("App listening at http://localhost:" + PORT);
-});
+// setup handelbars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
+// routes
+require("./routes/api-routes.js")(app);
+require("./routes/view-routes.js")(app);
+
+// start the server
+db.sequelize.sync({ force: true }).then(function () {
+    app.listen(PORT, function () {
+        console.log("App listening at http://localhost:" + PORT);
+    });
+});
