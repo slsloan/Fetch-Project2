a29fd7e3d0a54db2972ceedacea46f050f00f71e
Fidika <fidika@gmail.com>
2020-07-15 10:24:11 -0600

    how are my files being modified

--- config/config.js
+++ config/config.js
@@ -9,10 +9,10 @@ module.exports = {
         dialect: 'mysql',
     },
     production: {
-        username: process.env.JAWSDB_USERNAME,
-        password: process.env.JAWSDB_PASSWORD,
-        database: process.env.JAWSDB_DATABASE,
-        host: process.env.JAWSDB_HOST,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        host: process.env.DB_HOST,
         dialect: 'mysql',
-    }
+    },
 }
